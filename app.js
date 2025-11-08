59a4f246ed47b3702858a3abd1312434d1dce355
NRG55 <maloj_1980@yahoo.fr>
2025-11-08 16:55:49 +0000

    Add(app.js): index.js (node server) is rewritten with a backend framework Express in app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000);
+
+app.get('/', (req, res) => {
+    res.sendFile('./pages/index.html', {root: __dirname});
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile('./pages/about.html', {root: __dirname});
+});
+
+app.get('/contact-me', (req, res) => {
+    res.sendFile('./pages/contact-me.html', {root: __dirname});
+});
+
+app.use((req, res) => {
+    res.status(404).sendFile('./pages/404.html', {root: __dirname});
+});
--- index.js
+++ index.js
@@ -35,6 +35,6 @@ const server = http.createServer((req, res) => {
     });
 });
 
-server.listen(3000, 'localhost', () => {
+server.listen(8080, 'localhost', () => {
     console.log("Listening for request");
 });
